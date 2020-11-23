315d990c9e75016acad3fd06d4675938ba168676
Paul <423357+toothbrush@users.noreply.github.com>
2020-11-23 11:13:23 +0930

    Initial Commit

---
+++ landscape-ios-bug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  landscape-ios-bug
+//
+//  Created by Paul on 23/11/20.
+//  Copyright © 2020 Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
